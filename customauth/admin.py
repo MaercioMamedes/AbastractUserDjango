1306d35f635541fe7f122650344e2a961a1bcc3a
Maercio Mamedes <maerciomamedes@hotmail.com>
2025-06-03 19:56:13 -0300

    projeto finalizado

--- customauth/admin.py
+++ customauth/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import MyUser
 
-# Register your models here.
+
+class Meta:
+    model = MyUser
+    fields = "_all_"
+
+admin.site.register(MyUser)
--- customauth/models.py
+++ customauth/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class MyUser(AbstractUser):
+    """A classe MysUser irá herdar todos os atributos e métodos de um UserAdmin padrão do Django"""
+    phone = models.CharField("telefone", max_length=11, blank=True)
+    profile_image = models.ImageField(blank=True)
